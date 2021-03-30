1939ac88a5684e5ac5e3811ffe0844c01665ac7e
VFranco <vfranco@frd.org.mx>
2021-03-29 18:33:23 -0600

    Se agregaron los cors

--- servidor.js
+++ servidor.js
@@ -1,23 +1,34 @@
 const express = require('express');
 const http = require('http');
+const cors = require('cors');
 var app = express();
+
+app.use(cors());
 
 const servidor = http.createServer(app);
 
-const io = require('socket.io')(servidor, {
-    cors: {
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"],
-      allowedHeaders: ["Access-Control-Allow-Origin"],
-      credentials: false
-    }
-  });
+const socketio = require('socket.io');
+const io = socketio(servidor, {cors: {
+    origin: (origin, cb) => {
+        cb(null, origin);
+    },
+    methods: ["GET", "POST"]
+  }});
 
-
+let nombreUsuario = "";
 io.on('connection', socket => {
-    socket.on('conectado', () => {
-        console.log('Usuario Conectado');
+    socket.on('conectado', (nombre) => {
+        nombreUsuario = nombre;
+        socket.broadcast.emit('mensajes', {nombre: "Servidor", mensaje: `${nombreUsuario} se ha unido a la sala de chat.`});
     })
+
+    socket.on('mensaje', (nombre, mensaje) => {
+        io.emit('mensajes', {nombre, mensaje});
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('mensajes', {nombre: "Servidor", mensaje: `Un participante abandonó la sala de chat.`});
+    });
 })
 
 servidor.listen(3000, () => console.log('Servidor Inicializado'));
